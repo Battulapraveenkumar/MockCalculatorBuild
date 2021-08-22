a1a4f1044444372ad8a7ce565ef0918e9e951f11
Battulapraveenkumar <praveenkumarbattula8@gmail.com>
2021-08-22 20:29:44 +0530

    Calculator implementation and Testing

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator calc = null;
+    @BeforeEach
+    void assignedObject(){
+        calc = new Calculator();
+    }
+
+    @Test
+    void toComputeSumOfTwoDoubleInputs(){
+        assertEquals(8.0,calc.sum(4.0,4.0));
+    }
+
+    @Test
+    void toComputeDifferenceBetweenTwoDoubleInputs(){
+        assertEquals(2.0,calc.difference(4.0,2.0));
+    }
+
+    @Test
+    void toComputeProductOfTwoDoubleInputs(){
+        assertEquals(8.0,calc.product(4.0,2.0));
+    }
+
+    @Test
+    void toComputeQuotientWhenDividingTwoDoubleInputs(){
+        assertEquals(2.0,calc.division(4.0,2.0));
+    }
 }

8693ea2f65e49cd3808aeb921b8ff35103f58dcb
Battulapraveenkumar <praveenkumarbattula8@gmail.com>
2021-08-22 20:53:39 +0530

    Caluculator Implementation and Testing using Mockito

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,32 +1,43 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 public class CalculatorTest {
     Calculator calc = null;
+    CalculatorService calculatorService = mock(CalculatorService.class);
     @BeforeEach
     void assignedObject(){
-        calc = new Calculator();
+        calc = new Calculator(calculatorService);
     }
 
     @Test
-    void toComputeSumOfTwoDoubleInputs(){
+    void toCalculateSumOfTwoDoubleInputs(){
+        when(calculatorService.sum(4.0,4.0)).thenReturn(8.0);
         assertEquals(8.0,calc.sum(4.0,4.0));
+        verify(calculatorService).sum(4.0,4.0);
     }
 
     @Test
-    void toComputeDifferenceBetweenTwoDoubleInputs(){
+    void toCalculateDifferenceBetweenTwoDoubleInputs(){
+        when(calculatorService.difference(4.0,2.0)).thenReturn(2.0);
         assertEquals(2.0,calc.difference(4.0,2.0));
+        verify(calculatorService).difference(4.0,2.0);
     }
 
     @Test
-    void toComputeProductOfTwoDoubleInputs(){
+    void toCalculateProductOfTwoDoubleInputs(){
+        when(calculatorService.product(4.0,2.0)).thenReturn(8.0);
         assertEquals(8.0,calc.product(4.0,2.0));
+        verify(calculatorService).product(4.0,2.0);
     }
 
     @Test
-    void toComputeQuotientWhenDividingTwoDoubleInputs(){
+    void toCalculateQuotientWhenDividingTwoDoubleInputs(){
+        when(calculatorService.division(4.0,2.0)).thenReturn(2.0);
         assertEquals(2.0,calc.division(4.0,2.0));
+        verify(calculatorService).division(4.0,2.0);
     }
 }
